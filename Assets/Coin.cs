b96aecd8c4ddc588895fcd6f8ac33e9e0b9b34eb
jacobprimehd <35090895+jacobprimehd@users.noreply.github.com>
2023-03-14 11:56:45 -0700

    added currency

--- Assets/Coin.cs
+++ Assets/Coin.cs
@@ -20,6 +20,7 @@ public class Coin : MonoBehaviour
         if(other.gameObject.CompareTag("Player"))
         {
           Destroy(gameObject);
+          CoinCounter.instance.IncreaseCoins(value);
         }
     }
 }
--- Assets/CoinCounter.cs
+++ Assets/CoinCounter.cs
@@ -1,18 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CoinCounter : MonoBehaviour
 {
+    public static CoinCounter instance;
+    public TMP_Text coinText;
+    public int currentCoins = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        coinText.text = "Dollars: " + currentCoins.ToString();
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public void IncreaseCoins(int v)
+    {
+        currentCoins += v;
+        coinText.text = "Dollars: " + currentCoins.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
